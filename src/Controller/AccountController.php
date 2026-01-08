d85c3da4a082a1e9c10ea67c3b1bce30f5022dab
Paul von Berg <paul425@schule.bremen.de>
2026-01-08 16:39:59 +0100

    Add Profile Editor

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\EditProfileType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,6 +22,43 @@ final class AccountController extends AbstractController
         return $this->render('account/index.html.twig', [
             'user' => $user,
             'upcomingEvents' => $events,
+        ]);
+    }
+
+    #[Route('/account/events', name: 'app_account_events')]
+    public function events(UserRepository $ur): Response
+    {
+        $user = $ur->find($this->getUser()->getId());
+        $events = $user->getSavedEvents();
+
+        return $this->render('account/events.html.twig', [
+            'user' => $user,
+            'events' => $events,
+        ]);
+    }
+
+    #[Route('/account/edit', name: 'app_account_edit')]
+    public function edit(
+        Request $request,
+        EntityManagerInterface $em,
+    ): Response {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(EditProfileType::class, $user->getUserprofile());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Profile updated successfully.');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Event;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
